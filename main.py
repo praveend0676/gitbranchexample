9c527875627482fae3ee6ddb608a091c188f3060
praveend0676 <praveendevisetti0676@gmail.com>
2026-09-23 19:04:08 +0530

    Add dashboard feature

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from login import login
 from profile import display_profile
+from dashboard import show_dashboard
+
 
 print("Git Branching Demo")
 print("Welcome to the Learning Platform")
@@ -9,3 +11,6 @@ print(login("admin", "1234"))
 
 print("\n=== Profile ===")
 display_profile("Rahul", "rahul@gmail.com")
+
+print("\n=== Dashboard ===")
+show_dashboard()
