97a8c80d9448f8b17472b7f65a8d9d804280e3c1
praveend0676 <praveendevisetti0676@gmail.com>
2026-09-23 19:03:47 +0530

    Add profile feature

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from login import login
+from profile import display_profile
 
 print("Git Branching Demo")
 print("Welcome to the Learning Platform")
 
 print("\n=== Login ===")
 print(login("admin", "1234"))
+
+print("\n=== Profile ===")
+display_profile("Rahul", "rahul@gmail.com")
